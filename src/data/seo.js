0335b8e4e07fe8034246184119cc321b778d7751
Qobiljon2011 <mamurovqobiljon63@gmail.com>
2024-06-04 20:31:10 +0500

    portfolio

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a frontend developer with expertise in Next.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a frontend developer with expertise in Next.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Mamurov", "Mamurov Q", "Mamurov Qobiljon"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Mamurov", "Mamurov Q", "Mamurov Qobiljon"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Mamurov", "Mamurov Q", "Mamurov Qobiljon"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Mamurov", "Mamurov Q", "Mamurov Qobiljon"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,66 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Qobiljon",
+		name: "Mamurov Qobiljon",
+		email: "qobiljonmamurov6@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Hello, I'm Frontend developer, and I study at school.",
+		description:
+			"I am a frontend developer with expertise in Next.js. I have experience in Tailwind and Bootstrap, secure and reliable web applications using various frameworks and technologies. I enjoy learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Mamurov Q. I live in Tashkent Uzbekistan, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Spotify-Clone",
+			description:
+				"In this project I made a spotify-clone and tis web-site to listen different musics",
+			logo: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+			linkText: "View Project",
+			link: "https://spotify-exam-7-month.netlify.app/",
+		},
+
+		{
+			title: "CRYPTOFOLIO",
+			description:
+				"This project for different crypto coins.In this project you can see a different coins and like them.",
+			logo: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png",
+			linkText: "View Project",
+			link: "https://exam-end.vercel.app/",
+		},
+
+		{
+			title: "Anex web-site",
+			description:
+				"This site for exam, and this is my favourite site in html.In this site I show a price of different tickets.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://cute-sunshine-15a1a2.netlify.app/",
+		},
+	],
+};
+
+export default INFO;
